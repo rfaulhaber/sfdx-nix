59ddb9ada0cb4bf0fd7281f88b037137ec94b305
Ryan Faulhaber <ryf@sent.as>
2024-04-13 22:31:15 -0400

    attempts at using mkYarnPackage and normal stdenv

--- flake.nix
+++ flake.nix
@@ -12,26 +12,74 @@
     flake-utils,
   }:
     flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-      nodePackages =
-        (pkgs.callPackage ./default.nix {
-          inherit pkgs system;
-          nodejs = pkgs.nodejs_18;
-        });
-      # TODO can I fix this with a derivation?
-      sfdx = nodePackages.sfdx-cli // {
-        name = "sfdx";
-        pname = "sfdx";
+      pkgs = import nixpkgs {
+        inherit system;
       };
-      sf = nodePackages."@salesforce/cli" // {
+
+      sf = let
         name = "sf";
-        pname = "sf";
-      };
+        version = "2.38.0";
+        src = pkgs.fetchFromGitHub {
+          owner = "salesforcecli";
+          repo = "cli";
+          rev = version;
+          hash = "sha256-D6RIEoZhh9LgbNyi88UZ8cjgnPJRjv7SxczgvCSQtYA=";
+        };
+        offlineCache = pkgs.fetchYarnDeps {
+          yarnLock = src + "/yarn.lock";
+          hash = "sha256-+2s9eGVyGlrGlmbPjNdGnSYf4IHzRCIkKZCXXj4tygI=";
+        };
+      in
+        pkgs.stdenv.mkDerivation {
+          inherit version src;
+          pname = name;
+          nativeBuildInputs = with pkgs; [nodejs_21 yarn fixup_yarn_lock prefetch-yarn-deps];
+          configurePhase = ''
+            export HOME=$PWD/yarn_home
+            yarn config --offline set yarn-offline-mirror ${offlineCache}
+          '';
+          buildPhase = ''
+            export HOME=$PWD/yarn_home
+            fixup-yarn-lock ./yarn.lock
+            chmod -R +rw $PWD/scripts
+            yarn --offline  install
+            chmod -R +rw $PWD/node_modules
+            $PWD/node_modules/.bin/sf-install
+          '';
+        };
+      #         pkgs.mkYarnPackage rec {
+      #           inherit version;
+      #           pname = name;
+      #           src = pkgs.fetchFromGitHub {
+      #             owner = "salesforcecli";
+      #             repo = "cli";
+      #             rev = version;
+      #             hash = "sha256-D6RIEoZhh9LgbNyi88UZ8cjgnPJRjv7SxczgvCSQtYA=";
+      #           };
+      #           offlineCache = pkgs.fetchYarnDeps {
+      #             yarnLock = src + "/yarn.lock";
+      #             hash = "sha256-+2s9eGVyGlrGlmbPjNdGnSYf4IHzRCIkKZCXXj4tygI=";
+      #           };
+      #           nativeBuildInputs = with pkgs; [nodejs_21 yarn fixup_yarn_lock prefetch-yarn-deps];
+      #           ignoreScripts = false;
+      #           yarnFlags = [ "--production=false" ];
+      #           # pkgConfig."@salesforce/cli".ignoreScripts = false;
+      #           pkgConfig."@salesforce/cli".postInstall = ''
+      #             echo "hello postinstall"
+      #             echo "yarn bin: $(yarn bin)"
+      #             export NODE_ENV="dev";
+      #             yarn --offline --frozen-lockfile --ignore-scripts --production=true install
+      #             export PATH=$PATH:$(yarn --offline bin)
+      #             cp -r ${src}/scripts .
+      #             ls ./node_modules/@salesforce
+      #             yarn --offline --frozen-lockfile build
+      # '';
+      # };
     in rec {
-      packages.sfdx = sfdx;
       packages.sf = sf;
       packages.default = packages.sf;
+      formatter = pkgs.alejandra;
       devShells.default =
-        pkgs.mkShell {buildInputs = [pkgs.node2nix];};
+        pkgs.mkShell {buildInputs = with pkgs; [node2nix nodejs_21];};
     });
 }
