bfd9984e19f08b35565b8ad8ba85d91cf92e41c0
Ryan Faulhaber <ryf@sent.as>
2024-04-14 16:23:14 -0400

    attempt at rewriting using mkderivation

--- flake.nix
+++ flake.nix
@@ -33,48 +33,31 @@
         pkgs.stdenv.mkDerivation {
           inherit version src;
           pname = name;
-          nativeBuildInputs = with pkgs; [nodejs_21 yarn fixup_yarn_lock prefetch-yarn-deps];
+          nativeBuildInputs = with pkgs; [nodejs yarn prefetch-yarn-deps];
           configurePhase = ''
             export HOME=$PWD/yarn_home
             yarn config --offline set yarn-offline-mirror ${offlineCache}
           '';
           buildPhase = ''
             export HOME=$PWD/yarn_home
+            export SF_HIDE_RELEASE_NOTES=true
             fixup-yarn-lock ./yarn.lock
             chmod -R +rw $PWD/scripts
-            yarn --offline  install
+            yarn --offline install --ignore-scripts
             chmod -R +rw $PWD/node_modules
-            $PWD/node_modules/.bin/sf-install
+            patchShebangs --build node_modules
+            yarn --offline run build
           '';
+
+          installPhase = ''
+            mkdir $out
+            mv node_modules $out/
+            mv dist $out/
+            mkdir -p $out/bin
+            mv bin/run.js $out/bin/sf
+            patchShebangs $out
+'';
         };
-      #         pkgs.mkYarnPackage rec {
-      #           inherit version;
-      #           pname = name;
-      #           src = pkgs.fetchFromGitHub {
-      #             owner = "salesforcecli";
-      #             repo = "cli";
-      #             rev = version;
-      #             hash = "sha256-D6RIEoZhh9LgbNyi88UZ8cjgnPJRjv7SxczgvCSQtYA=";
-      #           };
-      #           offlineCache = pkgs.fetchYarnDeps {
-      #             yarnLock = src + "/yarn.lock";
-      #             hash = "sha256-+2s9eGVyGlrGlmbPjNdGnSYf4IHzRCIkKZCXXj4tygI=";
-      #           };
-      #           nativeBuildInputs = with pkgs; [nodejs_21 yarn fixup_yarn_lock prefetch-yarn-deps];
-      #           ignoreScripts = false;
-      #           yarnFlags = [ "--production=false" ];
-      #           # pkgConfig."@salesforce/cli".ignoreScripts = false;
-      #           pkgConfig."@salesforce/cli".postInstall = ''
-      #             echo "hello postinstall"
-      #             echo "yarn bin: $(yarn bin)"
-      #             export NODE_ENV="dev";
-      #             yarn --offline --frozen-lockfile --ignore-scripts --production=true install
-      #             export PATH=$PATH:$(yarn --offline bin)
-      #             cp -r ${src}/scripts .
-      #             ls ./node_modules/@salesforce
-      #             yarn --offline --frozen-lockfile build
-      # '';
-      # };
     in rec {
       packages.sf = sf;
       packages.default = packages.sf;
